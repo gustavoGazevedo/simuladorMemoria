2917d5e311fc45f1b36f9cd7b1489f6405681a8d
Gustavo Azevedo <31808556+gustavoGazevedo@users.noreply.github.com>
2019-06-19 14:04:22 -0300

    commit

--- js/script.js
+++ js/script.js
@@ -1,5 +1,15 @@
 /* Autor: Gustavo Azevedo */
 
-function runProgram() {
-  var myFile = $('#arquivo').prop('files');
+function main() {
+  var myFile = $('#arquivo').prop('files')[0]
+  var memorySize = $('#tamanhoMemoria').val();
+  var algorithm = document.querySelector('input[name="algoritmo"]:checked').value;
+  var fileData = '';
+
+  var reader = new FileReader();
+  reader.onload = function() {
+    fileData = reader.result;
+    console.log(fileData);
+  };
+  reader.readAsText(myFile);
 }
